8e495e0e0d224ef21a5d194b2f48c8e376667d1e
davewburns <dave.w.burns@gmail.com>
2022-06-24 05:10:06 -0500

    Set up square render

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
